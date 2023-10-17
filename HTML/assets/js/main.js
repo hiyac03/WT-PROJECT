7bd8e87f586ce242aa3f2b3cc8785ff4db14cb91
Hiya Chhabra <119084576+hiyac03@users.noreply.github.com>
2023-10-17 22:34:12 +0530

    Add files via upload

---
+++ HTML/assets/js/main.js
@@ -0,0 +1,95 @@
+		
+$(function(){
+
+
+	 /* Contact form ajax Handler
+    ================================================*/
+
+    $(".ajax-form").on('submit', function() {
+    	var form = $(this);
+        var formURL = $(this).attr("action");
+        var postData = $(this).serializeArray();
+
+        $.ajax({
+            url: formURL,
+            type: 'POST',
+            data: postData,
+            dataType: 'json',
+
+            success:function(data, textStatus, jqXHR){
+
+                if(data.success==1){
+
+                    form.find(".alert").fadeOut();
+                    form.find(".alert-success").html(data.message);
+                    form.find(".alert-success").fadeIn(600);
+                    
+
+                }else{
+
+                	form.find(".alert").fadeOut();
+                    form.find(".alert-danger").html(data.message);
+                    form.find(".alert-danger").fadeIn(600);
+
+                }
+            },
+
+            error: function(jqXHR, textStatus, errorThrown)  { 
+                
+                console.log(errorThrown);
+            }
+
+        });
+            
+
+        return false;
+     })
+
+
+
+    /*
+	On scroll animations
+	================================================
+	*/
+
+
+    var $elems = $('.animate-onscroll');
+
+    var winheight = $(window).height();
+    var fullheight = $(document).height();
+ 
+    $(window).scroll(function(){
+        animate_elems();
+    });
+
+
+
+    function animate_elems() {
+
+	    wintop = $(window).scrollTop(); // calculate distance from top of window
+	 
+	    // loop through each item to check when it animates
+	    $elems.each(function(){
+	    	
+	      $elm = $(this);
+	 
+	      if($elm.hasClass('animated')) { return true; } // if already animated skip to the next item
+	 
+	      topcoords = $elm.offset().top; // element's distance from top of page in pixels
+	 
+	      if(wintop > (topcoords - (winheight*.75))) {
+	        // animate when top of the window is 3/4 above the element
+	        $elm.addClass('animated');
+	      }
+
+	    });
+
+	  } // end animate_elems()
+
+	
+
+});
+
+
+
+		
